3ff080247e97af320915104b86fde5ef9cee5361
leandro4silva <leandro94.a.s@gmail.com>
2023-02-07 20:06:39 -0300

    feat: add modal to register new habit

--- src/components/SummaryTable/index.tsx
+++ src/components/SummaryTable/index.tsx
@@ -1,8 +1,14 @@
+import { generateDatesFromYearBeginning } from "../../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "../HabitDay";
 
 const weekDays = [
     'D', 'S', 'T', 'Q', 'Q', 'S', 'S'
 ];
+
+const summaryDates = generateDatesFromYearBeginning();
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
 
 export function SummaryTable() {
     return (
@@ -18,7 +24,17 @@ export function SummaryTable() {
                 })}
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <HabitDay />
+                {summaryDates.map((date, index) => {
+                    return(
+                        <HabitDay key={index} />
+                    )
+                })}
+                {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((value, index)=>{
+                    return(
+                        <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+                    )
+                })}
+
             </div>
         </div>
     )
