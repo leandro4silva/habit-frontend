df7db2ec444c6e9a312c45d40a25b6956b8f5d2d
leandro4silva <leandro94.a.s@gmail.com>
2023-02-22 22:45:58 -0300

    fix: loading of  progressbar

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -5,14 +5,7 @@ interface ProgressBarProps{
     progress: number
 }
 
-export function ProgressBar(props: ProgressBarProps) {
-    const [progress, setProgress] = useState(0);
-
-    useEffect(() => {
-        const timer = setTimeout(() => setProgress(props.progress), 200);
-        return () => clearTimeout(timer);
-    }, []);
-
+export function ProgressBar({progress}: ProgressBarProps) {
     return (
         <Progress.Root className='mt-4 relative overflow-hidden bg-zinc-700 rounded-xl w-full h-3'>
             <Progress.Indicator
